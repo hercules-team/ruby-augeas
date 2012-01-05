790b2f94e200077ba50a5fd63864477fe6926ecd
Ionuț Arțăriși <iartarisi@suse.cz>
2012-01-05 11:29:07 +0100

    add copyright notes to tests

--- tests/tc_augeas.rb
+++ tests/tc_augeas.rb
@@ -1,5 +1,26 @@
-# These tests are run against the old deprecated module which is
-# instantiated by Augeas::open
+##
+#  Augeas tests
+#
+#  Copyright (C) 2011 Red Hat Inc.
+#  Copyright (C) 2011 SUSE LINUX Products GmbH, Nuernberg, Germany.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+#
+# Authors: David Lutterkort <dlutter@redhat.com>
+#          Ionuț Arțăriși <iartarisi@suse.cz>
+##
 
 require 'test/unit'
 
